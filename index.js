054cfbfffdd304d80ab236d67393139405eec1ec
HaojieZhang123 <haojiezhang123@gmail.com>
2025-04-15 13:16:08 +0200

    [UPD] print string in console based on used given arguments via terminal

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello Boolean!');
+// console.log('Hello Boolean!');
+
+const arguments = process.argv.slice(2);
+
+if (arguments.length === 0) {
+  console.log('No arguments provided!');
+}
+else{
+    let string = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        string += ' ' + arguments[i];
+    }
+    console.log(string);
+}
